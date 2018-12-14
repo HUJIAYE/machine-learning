b79a4f53e58594cc09fb4f1869dfb5800e1848aa
JIAYE <hujiaye.dlut@gmail.com>
2018-12-14 22:05:34 +0800

    add some code into BP.py

--- MLaPP/algorithms/BP.py
+++ MLaPP/algorithms/BP.py
@@ -29,26 +29,29 @@ def g(x, y):
 def BP(data, label, hidden_number, step, model = 'normal'):
     output_number = label.shape[1]
     input_number = data.shape[1]
-    # 初始化参数
+    # 初始化参数  
     hidden_threshold = np.random.rand(data.shape[0], hidden_number)
     output_threshold = np.random.rand(data.shape[0], output_number)
-    input_hidden_para = np.random.rand(hidden_number, input_number)
-    hidden_output_para = np.random.rand(output_number, hidden_number)
+    input_hidden_para = np.random.rand(data.shape[0], hidden_number, input_number)
+    hidden_output_para = np.random.rand(data.shape[0], output_number, hidden_number)
     #训练网络：
     if model == 'normal':
         alpha = np.zeros((data.shape[0], hidden_number))
         error = np.zeros((data.shape[0], 1))
         beta = np.zeros((data.shape[0], output_number))
         for i in range(data.shape[0]):
+            # input-hidden layer parameters
             for j in range(hidden_number):
-                hidden_x = np.sum(data[i] * input_hidden_para[j].T) - hidden_threshold[i, j]
+                hidden_x = np.sum(data[i] * input_hidden_para[i, j].T) - hidden_threshold[i, j]
                 alpha[i,j] = sigmoid(hidden_x)
+            # hidden-output layer parameters
             for m in range(output_number):
-                output_x = np.sum(alpha[i] * hidden_output_para[m].T) - output_threshold[i, m]
+                output_x = np.sum(alpha[i] * hidden_output_para[i, m].T) - output_threshold[i, m]
                 beta[i, m] = sigmoid(output_x)
             error[i] = 0.5 * np.sum((beta[i] - label)**2)
             #更新参数
-            delta_hidden_treshold = 
+            for j in range():
+                delta_w[i, j] = step * g(label[i], beta[i][j]) * alpha[i, j]
             
             accumulated_error = np.sum(error)
             print(error)
@@ -57,6 +60,6 @@ def BP(data, label, hidden_number, step, model = 'normal'):
     
    
 if __name__ == '__main__':
-    x_train, x_test, y_train, y_test = load_data('C:\\Users\\Jiaye\\Desktop\\Jupyter\\MLaPP\\dataset\\iris.data')
+    x_train, x_test, y_train, y_test = load_data('C:\\Users\\Jiaye\\Desktop\\machine-learning\\MLaPP\\dataset\\iris.data')
    #x_train.shape, x_test.shape, y_train.shape, y_test.shape： (90, 4) (60, 4) (90, 1) (60, 1)
     BP(x_train, y_train, 6, 0.01)
