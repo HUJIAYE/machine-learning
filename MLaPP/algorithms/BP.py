be05768b81a9b5f409fbe01367bbdefae83ce3eb
JIAYE <hujiaye.dlut@gmail.com>
2018-12-17 10:47:44 +0800

    modify the BP.py file

--- MLaPP/algorithms/BP.py
+++ MLaPP/algorithms/BP.py
@@ -1,11 +1,14 @@
 '''
 author = Jiaye
 '''
-
+#coding=utf-8
 import math
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 #将原始数据标签做成字典
 def iris_type(s):
@@ -21,51 +24,83 @@ def load_data(path):
 
 #定义激活函数
 def sigmoid(x):
-    return (math.exp(-x)+1)**(-1)
+    x = float(x)
+    try:
+        ans = math.exp(-x)+1
+    except OverflowError:
+        ans = float('inf')
+    ans_sig = 1/ans
+    return ans_sig
     
-def g(x, y):
-    return y(1-y)(x-y)
+def g_f(y_truth, y_pre):
+    return y_pre*(1-y_pre)*(y_truth-y_pre)
 
-def sum_calculate(x, y, z)
+
     
 def BP(data, label, hidden_number, step, model = 'normal'):
-    output_number = label.shape[1]
+    
+    label = [int(i) for i in label]
+    output_number = len(label)
     input_number = data.shape[1]
     # 初始化参数  
-    hidden_threshold = np.random.rand(data.shape[0], hidden_number)
-    output_threshold = np.random.rand(data.shape[0], output_number)
-    input_hidden_para = np.random.rand(data.shape[0], hidden_number, input_number)
-    hidden_output_para = np.random.rand(data.shape[0], output_number, hidden_number)
+    gama = np.random.rand(data.shape[0], hidden_number)
+    theta = np.random.rand(data.shape[0], output_number)
+    v = np.random.rand(data.shape[0], hidden_number, input_number)
+    w = np.random.rand(data.shape[0], output_number, hidden_number)
+    accumulated_error = []
+    instan_error = 1
+    max_epoch = 1000
     #训练网络：
-    if model == 'normal':
+    while instan_error >= 0.5 and max_epoch > 0:
         alpha = np.zeros((data.shape[0], hidden_number))
         error = np.zeros((data.shape[0], 1))
         beta = np.zeros((data.shape[0], output_number))
         delta_w = np.zeros((data.shape[0], output_number, hidden_number))
+        delta_theta = np.zeros((data.shape[0], output_number))
+        delta_gama = np.zeros((data.shape[0], hidden_number))
+        delta_v = np.zeros((data.shape[0], hidden_number, input_number))
         for i in range(data.shape[0]):
             # input-hidden layer parameters
+            
             for j in range(hidden_number):
-                hidden_x = np.sum(data[i] * input_hidden_para[i, j].T) - hidden_threshold[i, j]
+                hidden_x = 0
+                for k in range(input_number):
+                    hidden_x += data[i,k] * v[i, j, k]  
+                hidden_x = hidden_x - gama[i, j]
                 alpha[i,j] = sigmoid(hidden_x)
             # hidden-output layer parameters
             for m in range(output_number):
-                output_x = np.sum(alpha[i] * hidden_output_para[i, m].T) - output_threshold[i, m]
+                output_x = 0
+                for k in range(hidden_number):
+                    output_x += alpha[i,k] * w[i, m, k]
+                output_x = output_x - theta[i, m]
                 beta[i, m] = sigmoid(output_x)
             error[i] = 0.5 * np.sum((beta[i] - label)**2)
-            #更新参数
-            e[h] = np.zeros(hidden_number, 1)
+            #标准梯度
+            e = np.zeros((hidden_number, 1))
             for h in range(hidden_number):
                 w_g = 0
                 for j in range(output_number):
-                    w_g += g(label[i], beta[i, j]) * hidden_output_para[i, j, h]
+                    w_g += g_f(label[i], beta[i, j]) * w[i, j, h]
                 e[h] = w_g
             for j in range(output_number):
                 for h in range(hidden_number):
-                    delta_w[i, j, h] = step * g(label[i], beta[i][j]) * alpha[i, h]
-                    delta_theta[i, j] = - step * g(label[i], beta[i][j])
-                    delta_v[k, h] = step * e[h] * data[i, k]  
-            accumulated_error = np.sum(error)
-            print(error) 
+                    delta_w[i, j, h] = step * g_f(label[i], beta[i][j]) * alpha[i, h]
+                    delta_theta[i, j] = - step * g_f(label[i], beta[i][j])
+                    delta_gama[i, h] = - step * e[h]
+                    for k in range(input_number):
+                        delta_v[i, h, k] = step * e[h] * data[i, k]  
+            #更新参数
+            gama = gama + delta_gama
+            theta = theta + delta_theta
+            v = v + delta_v
+            w = w + delta_w
+            #计算平均误差
+            instan_error = np.mean(error)
+            accumulated_error.append(instan_error)
+        max_epoch -= 1
+        if max_epoch % 100 == 0:
+            print(instan_error)
             
             
     
@@ -73,4 +108,4 @@ def BP(data, label, hidden_number, step, model = 'normal'):
 if __name__ == '__main__':
     x_train, x_test, y_train, y_test = load_data('C:\\Users\\Jiaye\\Desktop\\machine-learning\\MLaPP\\dataset\\iris.data')
    #x_train.shape, x_test.shape, y_train.shape, y_test.shape： (90, 4) (60, 4) (90, 1) (60, 1)
-    BP(x_train, y_train, 6, 0.01)
+    BP(x_train, y_train, 10, 10)
